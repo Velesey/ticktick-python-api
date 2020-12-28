b152964dc45e8599f305744331c0b36930c5bcd9
velesey <velesey@gmail.com>
2020-12-28 20:12:03 +0600

    fix errors

--- ticktick_api.py
+++ ticktick_api.py
@@ -11,11 +11,12 @@ class TickTickAPI:
     def __init__(self, user_name: str, password: str):
         self._user_name = user_name
         self._password = password
-        self._client = Client(user_name, password)
+        self._client = Client()
         self._tasks = TaskService(self._client)
 
     def connect(self) -> bool:
         return self._client.connect(self._user_name, self._password)
 
+    @property
     def tasks(self):
         return self._tasks;
